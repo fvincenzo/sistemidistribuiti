9f01b9fdb69e472a0bd5b95a0bec1fc436d460b5
nicolas.tagliani <nicolas.tagliani@a20de9d2-5f38-0410-a777-75cbac79620a>
2007-09-14 15:22:04 +0000

    Initial import.

---
+++ DAA/src/main/Allocator.java
@@ -0,0 +1,12 @@
+/*
+ * Created on 07/giu/07
+ *
+ */
+package main;
+
+public interface Allocator {
+    
+    public void step(int job_weight);
+    
+
+}
---
+++ DAA/src/main/Optimal.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 07/giu/07
+ *
+ */
+package main;
+
+public class Optimal implements Allocator {
+
+    public void step(int job_weight) {
+        // TODO Implementare step
+        
+    }
+
+}
