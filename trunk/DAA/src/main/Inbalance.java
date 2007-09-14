ed91cc5f79e97c61a16f91894a4209aeb7c967c3
nicolas.tagliani <nicolas.tagliani@a20de9d2-5f38-0410-a777-75cbac79620a>
2007-09-14 15:22:04 +0000

    Initial import.

---
+++ trunk/DAA/src/main/Inbalance.java
@@ -0,0 +1,14 @@
+/*
+ * Created on 07/giu/07
+ *
+ */
+package main;
+
+public class Inbalance implements Allocator {
+
+    public void step(int job_weight) {
+        // TODO Implementare step
+        
+    }
+
+}
