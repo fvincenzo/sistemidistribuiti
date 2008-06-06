c30f17650abbda494b5173178f102d265b1ecf22
nicolas.tagliani <nicolas.tagliani@a20de9d2-5f38-0410-a777-75cbac79620a>
2008-06-06 16:28:40 +0000

    Initial import.

---
+++ AndroidContactClient/src/android/client/UserInterface.java
@@ -0,0 +1,19 @@
+package android.client;
+
+public interface UserInterface {
+
+	public abstract String getUsename();
+
+	public abstract String getPosition();
+
+	public abstract String getMobile();
+
+	public abstract String getHome();
+
+	public abstract String getWork();
+
+	public abstract String getMail();
+
+	public abstract String getIM();
+
+}
