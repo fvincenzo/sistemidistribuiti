f8b42a340da995f5284c55f3c77b219cc90479f7
nicolas.tagliani <nicolas.tagliani@a20de9d2-5f38-0410-a777-75cbac79620a>
2008-06-08 14:40:01 +0000

    Commit del progetto. Lieve modifica all'interfaccia per comodità di programmazione I comandi sulla classe non richiedono che gli si passi il nome dell'utente che li esegue. Il protocollo non cambia in nessun punto. Cambia solo l'interfaccia per comodità di implementazione.

---
+++ trunk/AndroidContactClient/src/android/client/ContactClient.java
@@ -0,0 +1,228 @@
+package android.client;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Vector;
+
+public class ContactClient implements ContactClientInterface {
+	private PrintWriter out;
+	private BufferedReader in;
+	private Socket socket;
+	private String username = "";
+
+	public ContactClient(){}
+
+	@Override
+	public void acceptFriend( String friendName) {
+		if (username != ""){
+			out.println("DENYFRIEND");
+			out.println(username);
+			out.println(friendName);
+			out.println("END");
+		}	
+	}
+
+
+
+	@Override
+	public boolean addFriend(String friendName) {
+		if (username != ""){
+			out.println("ADDFRIEND");
+			out.println(username);
+			out.println(friendName);
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				//TODO fare il parsing della stringa di ritorno
+
+				return false;
+			} catch (IOException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public void denyFriend(String friendName) {
+		if (username != ""){
+			out.println("DENYFRIEND");
+			out.println(username);
+			out.println(friendName);
+			out.println("END");
+		}	
+	}
+
+	@Override
+	public Vector<String> getFriends() {
+		if (username != ""){
+			out.println("GETFRIENDS");
+			out.println(username);
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				//TODO fare il parsing della stringa di ritorno
+
+				return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		return new Vector<String>();
+	}
+
+	@Override
+	public UserInterface getUserDetails(String friend) {
+		if (username != ""){
+			//TODO controllare se il comando si chiama così davvero
+			out.println("GETUSERSDETAILS");
+			out.println(username);
+			out.println(friend);
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				//TODO fare il parsing della stringa di ritorno
+
+				return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Vector<String> getUsers() {
+		if (username != ""){
+			out.println("GETUSERS");
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				//TODO fare il parsing della stringa di ritorno
+
+				return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		return new Vector<String>();
+	}
+
+	@Override
+	public boolean login(String uname, String pwd) {
+		out.println("LOGIN");
+		out.println(uname);
+		out.println(pwd);
+		out.println("END");
+		String ret;
+		try {
+			ret = in.readLine();
+			if (ret.contains("OK")){
+				this.username = uname;
+				return true;
+			}
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	@Override
+	public Vector<String> pendingFriends() {
+		if (username != ""){
+			out.println("PENDINGFRIENDS");
+			out.println(username);
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				//TODO fare il parsing della stringa di ritorno
+
+				return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		return new Vector<String>();
+	}
+
+	@Override
+	public boolean register(String uname, String pwd, String geo) {
+		out.println("REGISTER");
+		out.println(uname);
+		out.println(pwd);
+		//TODO controlare se effettivamente bisogna mettere il cancelletto dopo geo
+		out.println(geo);
+		out.println("END");
+		String ret;
+		try {
+			ret = in.readLine();
+			if (ret.contains("OK")){
+				this.username = uname;
+				return true;
+			}
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+
+	public boolean register(String uname, String pwd, float x_position, float y_position) {
+		out.println("REGISTER");
+		out.println(uname);
+		out.println(pwd);
+		//TODO controlare se effettivamente bisogna mettere il cancelletto dopo geo
+		out.println("geo:"+x_position+","+y_position);
+		out.println("END");
+		String ret;
+		try {
+			ret = in.readLine();
+			if (ret.contains("OK")){
+				this.username = uname;
+				return true;
+			}
+			return false;
+		} catch (IOException e) {
+			return false;
+		}
+
+	}
+
+	@Override
+	public boolean updatePosition( float x_position, float y_position) {
+		if (username != ""){
+			out.println("UPDATEPOSITION");
+			out.println(username);
+			out.println("geo:"+x_position+","+y_position);
+			//TODO controlare se effettivamente bisogna mettere il cancelletto dopo geo
+			out.println("END");
+			String ret;
+			try {
+				ret = in.readLine();
+				if (ret.contains("OK")){
+					return true;
+				}
+				return false;
+			} catch (IOException e) {
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public void connect(String server) throws UnknownHostException, IOException{
+		socket = new Socket(server, 4444);
+		out = new PrintWriter(socket.getOutputStream(), true);
+		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+}
--- trunk/AndroidContactClient/src/android/client/MyContatsClient.java
+++ trunk/AndroidContactClient/src/android/client/MyContatsClient.java
@@ -1,32 +1,53 @@
 package android.client;
 
+import SQLite.BusyHandler;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 //TODO: Controllare come usare la posizione gps e le operzioni sulla rubrica
-public class MyContatsClient extends Activity {
+
+public class MyContatsClient extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
 	private ContactClientInterface contactList;
+	
+	
+	
+	
+	Button login;
+	Button register;
 	
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         
-        setContentView(R.layout.login);
-        
-        final EditText username = (EditText)findViewById(R.id.user);
-        
-        final Button button = (Button) findViewById(R.id.login);
-        button.setOnClickListener(new Button.OnClickListener() {
-            public void onClick(View v) {
-//            	username.getText().toString();
-            	
-//            	contactList.login("Utente", "ciao");
-            }
-        });
+        setContentView(R.layout.main);
+        login = (Button) findViewById(R.id.Login);
+        register  = (Button) findViewById(R.id.Register);
+        login.setOnClickListener(this);
+        register.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View arg0) {
+		if (arg0 == (Button) findViewById(R.id.Login)){
+			EditText username = (EditText)findViewById(R.id.Username);
+			EditText password = (EditText)findViewById(R.id.Password);
+			
+//			contactList.login(username.getText().toString(), password.getText().toString());
+			startActivity(new Intent(Intent.VIEW_ACTION, getIntent().getData()));
+		}
+		if (arg0 == (Button) findViewById(R.id.Register)){
+			Log.i("MyContactsClient", "Hai premuto register");
+		}
+	}
 }
